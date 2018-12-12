b06938fd71c0c9517f7336646004b917bbc24e03
birla8319 <ankitbirla2000@gmail.com>
2018-12-12 15:15:38 +0530

    Create 3D_Vector.py
    Use this you can draw a 3D Vector Space with moving.

---
+++ 3D_Vector.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+soa = np.array([[0, 0, 1, 1, -2, 0], [0, 0, 2, 1, 1, 0],
+                [0, 0, 3, 2, 1, 0], [0, 0, 4, 0.5, 0.7, 0]])
+
+X, Y, Z, U, V, W = zip(*soa)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.quiver(X, Y, Z, U, V, W)
+ax.set_xlim([-1, 0.5])
+ax.set_ylim([-1, 1.5])
+ax.set_zlim([-1, 8])
+plt.show()
